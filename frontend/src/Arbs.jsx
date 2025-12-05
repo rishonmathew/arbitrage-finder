c18c180270984f82ea6dacbd2af28091d557d4e9
Rishon Mathew <your-email@example.com>
2025-12-05 20:55:39 +1000

    Fixed axios URL quotes

--- frontend/src/Arbs.jsx
+++ frontend/src/Arbs.jsx
@@ -7,7 +7,8 @@ export default function Arbs() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/arbs/all")
+    axios.get("https://arbitrage-finder-enkv.onrender.com/api/arbs/all")
+
 
       .then(res => {
         setArbs(res.data);
